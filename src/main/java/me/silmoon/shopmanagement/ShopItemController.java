026a3498353adc190a39ab99012822478517ba35
SilMOON <chenminghui8899@gmail.com>
2020-02-20 21:54:14 +0800

    add navigation bar and some backend functions

--- src/main/java/me/silmoon/shopmanagement/ShopItemController.java
+++ src/main/java/me/silmoon/shopmanagement/ShopItemController.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.Vector;
 
 @RestController
 public class ShopItemController {
@@ -16,24 +17,27 @@ public class ShopItemController {
     private ShopItemRepo shopItemRepo;
 
     @GetMapping("/")
-    public String getIndex(Model model) {
-        return "index";
+    public ModelAndView getIndex(Model model) {
+        return new ModelAndView("index");
     }
 
     @GetMapping("/itemList")
-    public ArrayList<ShopItem> listItems() {
-        ArrayList<ShopItem> itemList = new ArrayList<ShopItem>();
+    public Vector<ShopItem> listItems() {
+        Vector<ShopItem> itemList = new Vector<ShopItem>();
         for (ShopItem shopItem : shopItemRepo.findAll())
             itemList.add(shopItem);
         return itemList;
     }
 
-    @GetMapping("/items?title={itemName}")
-    public String searchItem(Model model) {
-        return "index";
+    @GetMapping("/search?title={itemTitle}")
+    public Vector<ShopItem> searchItem(@RequestParam("itemTitle") String itemTitle) {
+        Vector<ShopItem> resultList = new Vector<ShopItem>();
+        for (ShopItem shopItem : shopItemRepo.findAllByTitle(itemTitle))
+            resultList.add(shopItem);
+        return resultList;
     }
 
-    @PostMapping("/addItems")
+    @PostMapping("/addItem")
     public String addNewItem(@RequestParam String title, @RequestParam String description, @RequestParam Integer price,
                                            @RequestParam String item_image_url)
     {
@@ -46,11 +50,36 @@ public class ShopItemController {
         newItem.setPrice(price);
         newItem.setItem_image_url(item_image_url);
         Date now = new Date();
-        DateFormat dFormat = new SimpleDateFormat("yyyy-mm-ddTHH:mm:ss:SSSZ");  //todo: format check
+        DateFormat dFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
         newItem.setCreated_at(dFormat.format(now));
         newItem.setUpdated_at(dFormat.format(now));
 
         shopItemRepo.save(newItem);
+        return "redirect:/";
+    }
+
+    @PutMapping("/editItem?id={itemId}")
+    public String editItem(@RequestParam("itemId") Integer itemId, @RequestParam String title,
+                           @RequestParam String description, @RequestParam Integer price, @RequestParam String item_image_url)
+    {
+        if (shopItemRepo.findById(itemId).isEmpty())
+            return HttpStatus.NOT_FOUND.toString();
+
+        ShopItem editItem = shopItemRepo.findById(itemId).get();
+
+        if (title.length()>100 || description.length()>500)
+            return HttpStatus.NOT_FOUND.toString();
+
+        editItem.setTitle(title);
+        editItem.setDescription(description);
+        editItem.setPrice(price);
+        editItem.setItem_image_url(item_image_url);
+        Date now = new Date();
+        DateFormat dFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+        editItem.setCreated_at(dFormat.format(now));
+        editItem.setUpdated_at(dFormat.format(now));
+
+        shopItemRepo.save(editItem);
         return "redirect:/";
     }
 
--- src/main/java/me/silmoon/shopmanagement/ShopItemRepo.java
+++ src/main/java/me/silmoon/shopmanagement/ShopItemRepo.java
@@ -1,7 +1,16 @@
 package me.silmoon.shopmanagement;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
+import java.util.Vector;
 
+
+@Transactional
+@Repository
 public interface ShopItemRepo extends CrudRepository<ShopItem, Integer> {
+    @Query("from ShopItem where title like ?1")
+    public Iterable<ShopItem> findAllByTitle(String title);
 
 }
